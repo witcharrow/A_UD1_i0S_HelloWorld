f13b56e5105f30f772d5f9d0de37176c0e17b70d
user11394 <user11394@37-128-186-162.rdns.melbourne.co.uk>
2014-04-08 21:09:17 +0100

    Initial Commit

---
+++ Prueba/Prueba/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Prueba
+//
+//  Created by user11394 on 4/8/14.
+//  Copyright (c) 2014 Alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Prueba/Prueba/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Prueba
+//
+//  Created by user11394 on 4/8/14.
+//  Copyright (c) 2014 Alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
