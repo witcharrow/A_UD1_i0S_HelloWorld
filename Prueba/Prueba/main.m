d7b1cc58eb91d50fdbb4ca928b95188470c1c923
user11394 <user11394@37-128-186-162.rdns.melbourne.co.uk>
2014-04-09 22:31:52 +0100

    error ejemplo pagina 40

--- Prueba/Prueba/main.m
+++ Prueba/Prueba/main.m
@@ -9,9 +9,55 @@
 #import <UIKit/UIKit.h>
 
 #import "AppDelegate.h"
+#import "PixelInvertido.h"
 
 int main(int argc, char * argv[])
 {
+    printf("Hola Mundo");
+    
+    NSString *cadena1 = @"hola";
+    NSString *cadena2 = @"h";
+    cadena2 = [cadena2 stringByAppendingString:@"ola"];
+    NSLog(@"%@", cadena1);
+    NSLog(@"%@", cadena2);
+    if(cadena1==cadena2){
+        NSLog(@"son iguales");
+    }
+    else{
+        NSLog(@"son distintas");
+        if([cadena1 isEqualToString:cadena2]){
+            NSLog(@"comparacion correcta. Son iguales");
+        }
+        else{
+            NSLog(@"comparacion correcta. Son diferentes");
+        }
+    }
+    
+    Pixel *pixel = [[Pixel alloc] init];
+    [pixel situarEnOrigen];
+    
+    NSLog(@"X: %d, Y: %d",[pixel posX],[pixel posY]);
+    [pixel moverHorizontalmente:30];
+    NSLog(@"X: %d, Y: %d",[pixel posX],[pixel posY]);
+    [pixel moverHorizontalmente:10 yVerticalmente:200];
+    NSLog(@"X: %d, Y: %d",[pixel posX],[pixel posY]);
+    [pixel moverHorizontalmente:10 yVerticalmente:200];
+    NSLog(@"X: %d, Y: %d",[pixel posX],[pixel posY]);
+    
+    if ([pixel estaFueraDeLosLimites]){
+        NSLog(@"Está fuera de los limites ese pixel");
+    }
+    else{
+        NSLog(@"El pixel esta dentro de los límites");
+    }
+    //[pixel release];
+    
+    pixel = [[PixelInvertido alloc] init];
+    
+    [pixel situarEnOrigenInvertido];
+    
+    NSLog(@"X: %d, Y: %d",[pixel posX],[pixel posY]);
+    
     @autoreleasepool {
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
